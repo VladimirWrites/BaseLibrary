e77caeffbf21a3bb73decd77c6be666684bf392e
Vladimir Jovanovic <v.jovanovic@holidaypirates.com>
2017-04-18 11:38:00 +0200

    isFirstTime() and setFirstTime() extended so you can track any event

--- baselibrary/src/main/java/com/vlad1m1r/baselibrary/utils/StoreUtils.java
+++ baselibrary/src/main/java/com/vlad1m1r/baselibrary/utils/StoreUtils.java
@@ -3,22 +3,25 @@ package com.vlad1m1r.baselibrary.utils;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.vlad1m1r.baselibrary.R;
 
 import java.util.Locale;
+
+import static android.text.TextUtils.concat;
 
 /**
  * Created by vladimirjovanovic on 10/23/15.
  */
 public class StoreUtils {
 
-    private static final String KEY_LANGUAGE_CODE = "language_code";
+    protected static final String KEY_LANGUAGE_CODE = "language_code";
 
-    private static final String KEY_FIRST_TIME = "first_time";
+    protected static final String KEY_FIRST_TIME = "first_time";
 
-    private final SharedPreferences preferences;
-    private final Resources resources;
+    protected final SharedPreferences preferences;
+    protected final Resources resources;
 
     public StoreUtils(@NonNull Resources resources, @NonNull SharedPreferences preferences) {
         this.preferences = preferences;
@@ -48,13 +51,31 @@ public class StoreUtils {
         editPreferences.apply();
     }
 
+    public boolean isFirstTime(@Nullable String event) {
+        return this.preferences.getBoolean(
+                event != null ?
+                        KEY_FIRST_TIME.concat("_").concat(event) :
+                        KEY_FIRST_TIME,
+                true
+        );
+    }
+
     public boolean isFirstTime() {
-        return this.preferences.getBoolean(KEY_FIRST_TIME, true);
+        isFirstTime(null);
+    }
+
+    public void setFirstTime(String event, boolean isFirstTime) {
+        SharedPreferences.Editor editPreferences = this.preferences.edit();
+        editPreferences.putBoolean(
+                event != null ?
+                        KEY_FIRST_TIME.concat("_").concat(event) :
+                        KEY_FIRST_TIME,
+                isFirstTime
+        );
+        editPreferences.apply();
     }
 
     public void setFirstTime(boolean isFirstTime) {
-        SharedPreferences.Editor editPreferences = this.preferences.edit();
-        editPreferences.putBoolean(KEY_FIRST_TIME, isFirstTime);
-        editPreferences.apply();
+        setFirstTime(null, isFirstTime);
     }
 }
